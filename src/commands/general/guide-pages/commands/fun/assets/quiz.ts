7514c349bf95088d712f29df55b812457421c19b
Euni <vescalaw@gmail.com>
2019-02-17 05:32:57 +0800

    Docs: tweaked changelog link, added warning for quiz

--- src/commands/general/guide-pages/commands/fun/assets/quiz.ts
+++ src/commands/general/guide-pages/commands/fun/assets/quiz.ts
@@ -8,5 +8,9 @@ export default {
       name: 'This command is featured by Leveling System',
       value: 'Gain EXP rewards by using this command!'
     },
+    {
+      name: 'Warning',
+      value: 'This command will be locked to a server member until he/she finally responds to this command.'
+    },
   ]
 } as IDialog;
